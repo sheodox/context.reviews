c87660149791f4d1878c7745ba14004f12287e9d
sheodox <imagamer27@msn.com>
2019-01-04 16:45:42 -0600

    better quote trimming, don't include english only searches

--- util/trim.js
+++ util/trim.js
@@ -2,28 +2,47 @@ module.exports = phrase => {
     const remove = regex => {
         phrase = phrase.replace(regex, '');
     };
-    
     phrase = phrase.trim();
-    //dangling punctuation/symbols from the previous sentence
-    remove(/^[。、」]/);
-    //dangling quotes for the next sentence
-    remove(/「$/);
     
-    //unmatched quotes
-    const leadingQuote = /^「/,
-        trailingQuote = /」$/;
-    //starts with an opening quote but doesn't have any closing quotes, assume only part of a quote was selected, no need to keep in quotes
-    if (leadingQuote.test(phrase) && !phrase.includes('」')) {
-        remove(leadingQuote);
-    }
-    //same with closing quote
-    if (trailingQuote.test(phrase) && !phrase.includes('「')) {
-        remove(trailingQuote);
-    }
-    //if a full quote was selected only, no need for the quote marks
-    if (trailingQuote.test(phrase) && leadingQuote.test(phrase)) {
-        remove(leadingQuote);
-        remove(trailingQuote);
-    }
+    //dangling punctuation/symbols from the previous sentence
+    remove(/^[。、！]/);
+    //dangling commas
+    remove(/[、]$/);
+    
+    //if they look up something in english don't count that as a word to review (they can look up the result if they really want it to show up)
+    remove(/^[\w\d\s]*$/);
+
+
+    //remove messy copying (extra punctuation for other sentences), and remove quotes when they're not necessary for the quote.
+    //we don't want to remove quotes if it's something like "「うるさいだまれ」って言った" because it changes the meaning.
+    [   ['「', '」'],
+        ['『', '』'],
+        ['【', '】'],
+    ].forEach(([open, close]) => {
+        const leadingOpen = new RegExp('^' + open),
+            leadingClose = new RegExp('^' + close),
+            trailingClose = new RegExp(close + '$'),
+            trailingOpen = new RegExp(open + '$');
+
+        //starts with an opening quote but doesn't have any closing quotes, assume only part of a quote was selected, no need to keep in quotes
+        if (leadingOpen.test(phrase) && !phrase.includes(close)) {
+            remove(leadingOpen);
+        }
+        //same with closing quote
+        if (trailingClose.test(phrase) && !phrase.includes(open)) {
+            remove(trailingClose);
+        }
+        
+        //a full quote with quote marks was included and nothing outside it, just remove the quote, it's unnecessary
+        if (leadingOpen.test(phrase) && trailingClose.test(phrase)) {
+            remove(leadingOpen);
+            remove(trailingClose);
+        }
+        
+        //remove any quotes for quotes that weren't included in the phrase
+        remove(trailingOpen);
+        remove(leadingClose);
+    });
+    
     return phrase;
 };
