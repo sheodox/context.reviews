48c013e4f2aba3b38dd28149c3d24b43b4596bf8
sheodox <3468630+sheodox@users.noreply.github.com>
2020-05-10 15:06:32 -0500

    add buttons to each jisho search result to add them to the review list, particularly useful when the first result isn't the word you want, or you're doing a wildcard search and have to scroll a bit to find the word you're looking for

--- public-src/javascripts/jisho-phrase-stasher.user.js
+++ public-src/javascripts/jisho-phrase-stasher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jisho Phrase Stasher
 // @namespace    http://tampermonkey.net/
-// @version      0.0.6
+// @version      0.0.7
 // @description  Stores looked up phrases for later review
 // @author       sheodox
 // @match        https://jisho.org/*
@@ -30,6 +30,17 @@
             setServerURL,
             'u'
         );
+
+        //if the first result ever changes (like when clicking through parts of a sentence), run a callback
+        let lastKnownFirstResult = '';
+        setInterval(() => {
+            const firstResultText = $('.concept_light .concept_light-representation .text').eq(0).text().trim()
+
+            if (firstResultText !== lastKnownFirstResult) {
+                lastKnownFirstResult = firstResultText;
+                onResultsChange();
+            }
+        }, 20);
     }
     
     function setServerURL() {
@@ -41,10 +52,10 @@
             setup();
         }
     }
-    
-    function record() {
+
+    function record(word) {
         if (serverUrl) {
-            const phrase = document.querySelector('#keyword').value,
+            const phrase = word || document.querySelector('#keyword').value,
                 url = `${serverUrl}/add/${encodeURIComponent(phrase)}`;
             GM_xmlhttpRequest({
                 method: 'GET',
@@ -52,6 +63,35 @@
             });
         }
     }
+
+    //anything that needs to happen whenever the list of search results change
+    function onResultsChange() {
+        //.concept_light-wrapper is the column that contains the reading for the word, as well as the links
+        $('.concept_light').each(function() {
+            const $wrapper = $(this),
+                word = $wrapper.find('.concept_light-representation .text').text().trim(),
+                $addReview = $('<a />')
+                    .text(`Add ${word} to review list`)
+                    .attr({
+                        href: '#',
+                        class: 'concept_light-status_link'
+                    })
+                    .on('click', (e) => {
+                        record(word)
+                        //prevent scrolling to the top
+                        e.preventDefault();
+                    })
+
+            //.concept_light-status is the direct container of the badges and links beneath the word
+            $wrapper
+                //it's easy to accidentally open the "links" dropdown when moving your cursor down the list
+                //of links, so put the add button right before it so that remains on the bottom
+                .find('.concept_light-status a[data-dropdown]')
+                .before($addReview);
+
+        })
+    }
+
     setup();
     record();
 })();
