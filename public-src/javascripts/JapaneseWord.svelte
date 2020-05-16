f6a0c30c03a5683866b15455ad1e77f587faa8f2
sheodox <3468630+sheodox@users.noreply.github.com>
2020-05-16 14:13:48 -0500

    make the furigana get real big on hover, hard to differentiate between dakuten sometimes

--- public-src/javascripts/JapaneseWord.svelte
+++ public-src/javascripts/JapaneseWord.svelte
@@ -1,3 +1,18 @@
+<style>
+    @keyframes furigana-grow-delayed {
+        100% {
+            font-size: 1.2rem;
+        }
+	}
+    rt {
+        font-size: 11px;
+    }
+    ruby:hover rt {
+        animation: furigana-grow-delayed 0.25s forwards;
+        animation-delay: 2s;
+    }
+</style>
+
 <ruby>
     {word}
     {#if word !== reading && reading}
@@ -6,6 +21,7 @@
 		<rp>)</rp>
     {/if}
 </ruby>
+
 <script>
     export let word;
     export let reading;
