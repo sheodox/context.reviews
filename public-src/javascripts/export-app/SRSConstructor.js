e8970a8e7f1eab5c5d0abbb85f22d67ce57cf054
sheodox <3468630+sheodox@users.noreply.github.com>
2020-06-04 22:30:45 -0500

    get background and panel background colors directly from the css variables on the page, so the cards are created with styles that match the site's theme automatically

--- public-src/javascripts/export-app/SRSConstructor.js
+++ public-src/javascripts/export-app/SRSConstructor.js
@@ -22,132 +22,147 @@ export default class SRSConstructor {
 	}
 }
 
-const ankiCommonStyles = `
-		<style>
-			.card {
-				background: #151d29 !important;
-				color: white;
-				max-width: 500px;
-				margin: 0 auto;
-				font-family: "Source Han Sans", "源ノ角ゴシック", "Hiragino Sans", "HiraKakuProN-W3", "Hiragino Kaku Gothic ProN W3", "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", "Noto Sans", "Noto Sans CJK JP", "メイリオ", Meiryo, "游ゴシック", YuGothic, "ＭＳ Ｐゴシック", "MS PGothic", "ＭＳ ゴシック", "MS Gothic", sans-serif;
-			}
-			.word {
-				font-size: 3rem;
-			}
-			.reading {
-				font-size: 2rem;
-			}
-			small {
-				color: gray;
-			}
-			a {
-				color: #00b7ff;
-			}
-			li {
-				text-align: left;
-			}
-			.context {
-				font-size: 1.5rem;
-			}
-			.tag {
-				color: #8293a1;
-				border: 1px solid #8293a1;
-				font-size: 0.7rem;
-				padding: 0 0.2rem;
-				border-radius: 2px;
-			}
-			a.source {
-				text-transform: capitalize;
-				font-size: 0.7rem;
-			}
-			.tag.common {
-				color: #00ffac;
-				border-color: #00ffac;
-			}
-			.tag.wanikani {
-				border-color: #82216f;
-				color: white;
-				text-shadow: 0 0 1px black;
-			}
-			.tag:not(:last-of-type) {
-				margin-right: 0.3rem;
-			}
-			.alternate-forms ruby:not(:last-of-type) {
-				margin-right: 0.5rem;
-			}
-			.alternate-forms ruby:not(:last-of-type)::after {
-				content: ', ';
-			}
-			.dictionary-details {
-				padding: 0.5rem;
-				border-radius: 3px;
-				background: #232a3b;
-			}
-		</style>
-	`,
-	ankiFrontTemplate = Handlebars.compile(`
-		<p class="word">{{word}}</p>
-		${ankiCommonStyles}
-	`),
-	ankiBackTemplate = Handlebars.compile(`
-		{{#if showReading}}
-			<p class="reading">{{reading}}</p>
-		{{/if}}
-		
-		<div class="dictionary-details">
-			{{#if showOriginal}}
-				<p class="dictionary-form">
-					<ruby>
-						{{definition.word}}
-						<rp>(</rp>
-						<rt>{{definition.reading}}</rt>
-						<rp>)</rp>
-					</ruby>
-				</p>
+// build the templates when used so they for sure have access to the css variables by then,
+// if this runs as the file first runs the css won't have loaded and the background colors will be blank
+function createTemplates() {
+	const docStyles = getComputedStyle(document.documentElement),
+		getCSSVar = varName => docStyles.getPropertyValue(`--${varName}`),
+		ankiCommonStyles = `
+			<style>
+				.card {
+					background: ${getCSSVar('bg')} !important;
+					color: white;
+					max-width: 500px;
+					margin: 0 auto;
+					font-family: "Source Han Sans", "源ノ角ゴシック", "Hiragino Sans", "HiraKakuProN-W3", "Hiragino Kaku Gothic ProN W3", "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", "Noto Sans", "Noto Sans CJK JP", "メイリオ", Meiryo, "游ゴシック", YuGothic, "ＭＳ Ｐゴシック", "MS PGothic", "ＭＳ ゴシック", "MS Gothic", sans-serif;
+				}
+				.word {
+					font-size: 3rem;
+				}
+				.reading {
+					font-size: 2rem;
+				}
+				small {
+					color: gray;
+				}
+				a {
+					color: #00b7ff;
+				}
+				li {
+					text-align: left;
+				}
+				.context {
+					font-size: 1.5rem;
+				}
+				.tag {
+					color: #8293a1;
+					border: 1px solid #8293a1;
+					font-size: 0.7rem;
+					padding: 0 0.2rem;
+					border-radius: 2px;
+				}
+				a.source {
+					text-transform: capitalize;
+					font-size: 0.7rem;
+				}
+				.tag.common {
+					color: #00ffac;
+					border-color: #00ffac;
+				}
+				.tag.wanikani {
+					border-color: #82216f;
+					color: white;
+					text-shadow: 0 0 1px black;
+				}
+				.tag:not(:last-of-type) {
+					margin-right: 0.3rem;
+				}
+				.alternate-forms ruby:not(:last-of-type) {
+					margin-right: 0.5rem;
+				}
+				.alternate-forms ruby:not(:last-of-type)::after {
+					content: ', ';
+				}
+				.dictionary-details {
+					padding: 0.5rem;
+					border-radius: 3px;
+					background: ${getCSSVar('panel-bg')};
+				}
+			</style>
+		`,
+		ankiFrontTemplate = Handlebars.compile(`
+			<p class="word">{{word}}</p>
+			${ankiCommonStyles}
+		`),
+		ankiBackTemplate = Handlebars.compile(`
+			{{#if showReading}}
+				<p class="reading">{{reading}}</p>
 			{{/if}}
 			
-			{{#if definition.tags}}
-				{{#each definition.tags}}
-					<span class="tag {{this.type}}" style="{{this.styles}}">{{this.text}}</span>
-				{{/each}}
-			{{/if}}
-			<ol>
-				{{#each definition.meanings}}
-					<li>
-						{{#if this.preInfo}}
-							<small>{{this.preInfo}}</small>
-							<br>
-						{{/if}}
-						{{this.definition}}
-						{{#if this.info}}
-							<small> {{this.info}}</small>
-						{{/if}}
-					</li>
-				{{/each}}
-			</ol>
-			{{#if definition.alternateForms}}
-				<small class="alternate-forms">
-					Alternate forms: 
-					{{#each definition.alternateForms}}
+			<div class="dictionary-details">
+				{{#if showOriginal}}
+					<p class="dictionary-form">
 						<ruby>
-							{{this.word}}
-							{{#if this.reading}}
-								<rp>(</rp>
-								<rt>{{this.reading}}</rt>
-								<rp>)</rp>
-							{{/if}}
+							{{definition.word}}
+							<rp>(</rp>
+							<rt>{{definition.reading}}</rt>
+							<rp>)</rp>
 						</ruby>
+					</p>
+				{{/if}}
+				
+				{{#if definition.tags}}
+					{{#each definition.tags}}
+						<span class="tag {{this.type}}" style="{{this.styles}}">{{this.text}}</span>
 					{{/each}}
-				</small>
+				{{/if}}
+				<ol>
+					{{#each definition.meanings}}
+						<li>
+							{{#if this.preInfo}}
+								<small>{{this.preInfo}}</small>
+								<br>
+							{{/if}}
+							{{this.definition}}
+							{{#if this.info}}
+								<small> {{this.info}}</small>
+							{{/if}}
+						</li>
+					{{/each}}
+				</ol>
+				{{#if definition.alternateForms}}
+					<small class="alternate-forms">
+						Alternate forms: 
+						{{#each definition.alternateForms}}
+							<ruby>
+								{{this.word}}
+								{{#if this.reading}}
+									<rp>(</rp>
+									<rt>{{this.reading}}</rt>
+									<rp>)</rp>
+								{{/if}}
+							</ruby>
+						{{/each}}
+					</small>
+				{{/if}}
+			</div>
+			{{#if context}}
+				<p class="context">Context: 「{{context}}」</p>
 			{{/if}}
-		</div>
-		{{#if context}}
-			<p class="context">Context: 「{{context}}」</p>
-		{{/if}}
-		<a href="{{definition.href}}" class="source">Definition source: {{source}}</a>
-	`)
+			<a href="{{definition.href}}" class="source">Definition source: {{source}}</a>
+		`);
+
+	return [
+		ankiFrontTemplate,
+		ankiBackTemplate
+	];
+}
 
 export function compileAnkiCard(c) {
+	const [
+		ankiFrontTemplate,
+		ankiBackTemplate
+	] = createTemplates();
 	//don't leak changes if things weren't cloned elsewhere
 	const card = cloneObject(c);
 	//some processing beforehand to make the template easier to write
