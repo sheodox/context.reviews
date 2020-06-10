c483884a445e3d95aa15bd66a27b8228de1ff824
sheodox <3468630+sheodox@users.noreply.github.com>
2020-06-10 15:04:57 -0500

    add 'overflow-y: scroll' to the body in the export app. it'll almost always have a scrollbar so forcing it on prevents the page from shifting side to side whenever the definitions load

--- public-src/javascripts/export-app/export-app.js
+++ public-src/javascripts/export-app/export-app.js
@@ -1,5 +1,6 @@
 import App from './ExportApp.svelte';
 import style from '../../stylesheets/style.scss';
+import exportStyles from '../../stylesheets/export.scss';
 
 const app = new App({
 	target: document.querySelector('#app-root')
