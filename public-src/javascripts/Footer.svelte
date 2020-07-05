98e3c42b05b0b8b4a576a6eeac162f083387102b
sheodox <3468630+sheodox@users.noreply.github.com>
2020-07-04 21:55:40 -0500

    add an email link in the footer, plus some icons.

--- public-src/javascripts/Footer.svelte
+++ public-src/javascripts/Footer.svelte
@@ -1,14 +1,30 @@
 <style>
-    footer{
+    footer {
         width: 100%;
         position: relative;
         bottom: 0;
         padding: 1rem;
         background: var(--panel-footer-bg);
         border-top: 1px solid var(--accent-purple);
+        display: flex;
+        justify-content: center;
+    }
+    footer a + a {
+        margin-left: 1rem;
     }
 </style>
 
 <footer>
-    <a href="/auth/logout">Sign out</a>
+    <a href="/auth/logout">
+        <Icon icon="login" />
+        Sign Out
+    </a>
+    <a href="mailto:help@context.reviews">
+        <Icon icon="email" />
+        Help & Feedback
+    </a>
 </footer>
+
+<script>
+    import Icon from './Icon.svelte';
+</script>
