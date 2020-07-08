678e3537a26637e4b8c0a580087a83c4b5f7bcd3
sheodox <3468630+sheodox@users.noreply.github.com>
2020-07-07 22:23:22 -0500

    added a new font for all headers, and reduced the number of background colors present on the landing page. fewer colors looks more cohesive and modern.

--- public-src/javascripts/landing/Pitch.svelte
+++ public-src/javascripts/landing/Pitch.svelte
@@ -1,6 +1,7 @@
 <style>
     .panel {
         margin: 1rem;
+		padding: 2rem;
     }
     h2 {
         margin: 0;
@@ -26,6 +27,7 @@
 	.row {
 		display: flex;
 		align-items: start;
+		margin-bottom: 2rem;
 	}
 	.row > * {
 		flex: 1;
@@ -36,54 +38,50 @@
 			align-items: center;
 		}
 	}
-	h1 {
+	h2 {
 		font-size: 2.5rem;
 		text-align: center;
 	}
     p {
-		line-height: 1.5;
+		line-height: 1.7;
+		text-align: center;
 	}
 </style>
 
-<h1>Make Any Japanese Text a Study Resource</h1>
-<div class="row">
-	<div class="panel">
-		<div class="header">
-			<h2>Easily Study New Words</h2>
-		</div>
-		<div class="panel-body">
-			<p>
-				Context.Reviews aims to make studying Japanese more interesting by helping you learn the vocabulary
-				you encounter. Words you encounter in native material are more memorable than drilling pre-made
-				decks, not to mention more fun!
-			</p>
-			<p>
-				As you're reading any Japanese material of your choosing, look up sentences containing words you don't yet know
-				on <ExternalLink href="https://jisho.org">Jisho.org</ExternalLink>, then come back here to quickly create
-				a beautiful Anki deck out of everything you just learned!
-			</p>
-		</div>
-	</div>
-
-	<aside class="outlined">
-		<DemoCard />
-	</aside>
-</div>
-
 <div class="panel">
-        <h2 class="header">
-			Anki Card Builder Wizard
-		</h2>
-	<div class="panel-body">
-		<p>
-			Create Anki cards just by selecting the unknown words in each context sentence you looked up.
-		</p>
-		<p>
-			Making good Anki cards takes time, wizard should help make it a lot easier. Definitions are automatically pulled
-			from Jisho, and your context sentence is always included on the card.
-		</p>
-		<video muted autoplay loop controls src="/videos/anki-export-demo.mp4" class="outlined" />
+	<div class="row">
+		<div>
+			<h2>Study Japanese Using Anything</h2>
+			<div class="panel-body">
+				<p>
+					Context.Reviews aims to make studying Japanese more interesting by helping you learn the vocabulary
+					you encounter. Words you encounter in native material are more memorable than drilling pre-made
+					decks, not to mention more fun!
+				</p>
+				<p>
+					As you're reading any Japanese material of your choosing, look up sentences containing words you don't yet know
+					on <ExternalLink href="https://jisho.org">Jisho.org</ExternalLink>, then come back here to quickly create
+					a beautiful Anki deck out of everything you just learned!
+				</p>
+			</div>
+		</div>
+
+		<aside class="outlined">
+			<DemoCard />
+		</aside>
 	</div>
+
+	<h2>
+		Anki Card Wizard
+	</h2>
+	<p>
+		Create Anki cards just by selecting the unknown words in each context sentence you looked up.
+	</p>
+	<p>
+		Making good Anki cards takes time, wizard should help make it a lot easier. Definitions are automatically pulled
+		from Jisho, and your context sentence is always included on the card.
+	</p>
+	<video muted autoplay loop controls src="/videos/anki-export-demo.mp4" class="outlined" />
 </div>
 
 <script>
