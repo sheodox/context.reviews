1b904cc586976fe453bd71cb6f9c5877ff6858c8
sheodox <3468630+sheodox@users.noreply.github.com>
2020-07-14 22:06:08 -0500

    add a build script for packaging the extension using web-ext, and don't include files meant for the website when building the script. since the extension should always use the most readable code possible it's build with '--mode development' and a new webpack env argument is included to differentiate between which dev/prod sites in the extension

--- webpack.config.js
+++ webpack.config.js
@@ -4,89 +4,83 @@ const path = require('path'),
 	isProd = process.argv.includes('production'),
 	buildExtension = require('./extension-src/build');
 
-module.exports = [{
-	name: 'website',
-	watch: !isProd,
-	mode: isProd ? 'production' : 'development',
-	entry: {
-		list: './public-src/javascripts/list-app/list-app.js',
-		export: './public-src/javascripts/export-app/export-app.js',
-		landing: './public-src/javascripts/landing/landing-app.js',
-	},
-	output: {
-		filename: '[name].[contenthash].js',
-		path: path.resolve(__dirname, './public')
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
+module.exports = env => {
+	return [{
+		name: 'website',
+		mode: isProd ? 'production' : 'development',
+		entry: {
+			list: './public-src/javascripts/list-app/list-app.js',
+			export: './public-src/javascripts/export-app/export-app.js',
+			landing: './public-src/javascripts/landing/landing-app.js',
 		},
-		extensions: ['.mjs', '.js', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(html|svelte)$/,
-				exclude: /node_modules/,
-				use: 'svelte-loader'
+		output: {
+			filename: '[name].[contenthash].js',
+			path: path.resolve(__dirname, './public')
+		},
+		resolve: {
+			alias: {
+				svelte: path.resolve('node_modules', 'svelte')
 			},
-			{
-				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
+			extensions: ['.mjs', '.js', '.svelte'],
+			mainFields: ['svelte', 'browser', 'module', 'main'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(html|svelte)$/,
+					exclude: /node_modules/,
+					use: 'svelte-loader'
+				},
+				{
+					test: /\.scss$/,
+					use: ['style-loader', 'css-loader', 'sass-loader']
+				}
+			]
+		},
+		plugins: [
+			new CopyPlugin([{
+				from: '**.png',
+				context: './public-src',
+			},
+				{from: '**/*.user.js', context: './public-src'},
+				{from: '**/*.mp4', context: './public-src'},
+			]),
+			new ManifestPlugin()
 		]
-	},
-	plugins: [
-		new CopyPlugin([{
-			from: '**.png',
-			context: './public-src',
+	}, {
+		name: 'extension',
+		// don't minify the extension code
+		mode: 'development',
+		devtool: 'hidden-source-map',
+		entry: {
+			'phrase-stasher': './extension-src/phrase-stasher.js',
 		},
-			{from: '**/*.user.js', context: './public-src'},
-			{from: '**/*.mp4', context: './public-src'},
-		]),
-		new ManifestPlugin()
-	]
-}, {
-	name: 'extension',
-	watch: !isProd,
-	// don't minify the extension code
-	mode: 'development',
-	devtool: 'hidden-source-map',
-	entry: {
-		'phrase-stasher': './extension-src/phrase-stasher.js',
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, './extension')
-	},
-	resolve: {
-		alias: {
-			'vue$': 'vue/dist/vue.esm.js'
-		}
-	},
-	module: {
-		rules: [
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, './extension')
+		},
+		resolve: {
+			alias: {
+				'vue$': 'vue/dist/vue.esm.js'
+			}
+		},
+		module: {
+			rules: [
+				{
+					test: /\.scss$/,
+					use: ['style-loader', 'css-loader', 'sass-loader']
+				}
+			]
+		},
+		plugins: [
 			{
-				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
+				apply: compiler => {
+					compiler.hooks.afterEmit.tap('UserscriptTweaks', compilation => {
+						buildExtension(env.SITE_TARGET === 'prod');
+					})
+				}
+			},
+			new ManifestPlugin()
 		]
-	},
-	plugins: [
-		new CopyPlugin([{
-			from: '**.png',
-			context: './public-src',
-		},
-			{from: '**/*.user.js', context: './public-src'},
-			{from: '**/*.mp4', context: './public-src'},
-		]), {
-			apply: compiler => {
-				compiler.hooks.afterEmit.tap('UserscriptTweaks', compilation => {
-					buildExtension(isProd);
-				})
-			}
-		},
-		new ManifestPlugin()
-	]
-}];
+	}];
+}
