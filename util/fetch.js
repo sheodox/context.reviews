1cc3a1df1ee0eb758992d32a9a9996964dcc1741
sheodox <3468630+sheodox@users.noreply.github.com>
2020-07-14 20:03:26 -0500

    use a more responsible user agent. a 'real' user agent was used for Goo searches (i think) but those aren't in use anymore so there's a more relevant header ("Context.Reviews (https://context.reviews)") to identify the app instead of an outdated Chrome user agent string.

--- util/fetch.js
+++ util/fetch.js
@@ -13,7 +13,7 @@ function fetch(url) {
             method: 'GET',
             agent: false,
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36p'
+                'User-Agent': 'Context.Reviews (https://context.reviews)'
             }
         }, res => {
             let body = '';
