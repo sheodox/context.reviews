bf8f3414e1b73b345601c7a47ff89ab3fded9d30
sheodox <3468630+sheodox@users.noreply.github.com>
2020-07-16 21:01:26 -0500

    stretch the background size of the progress bar so its gradient matches with the gradient containing it. also put 100% width on the progress element itself because in chrome it wasn't extending to the full width and looked really confusing (it looked like the second half of the bar was filled up already)

--- public-src/javascripts/export-app/CardProgress.svelte
+++ public-src/javascripts/export-app/CardProgress.svelte
@@ -14,6 +14,15 @@
 		border-radius: 0.2rem;
 		flex: 1;
 	}
+	progress {
+		width: 100%;
+	}
+	progress::-moz-progress-bar {
+		background-size: var(--progress-bg-size) 100%;
+	}
+	progress::-webkit-progress-value {
+		background-size: var(--progress-bg-size) 100%;
+	}
 </style>
 
 <div>
@@ -21,7 +30,12 @@
 		Phrases processed ({processedPhrases}/{totalPhrases})
 	</label>
     <div class="progress-border">
-		<progress id="processed-phrases" value={processedPhrases} max={totalPhrases}></progress>
+		<progress
+			id="processed-phrases"
+			value={processedPhrases}
+			max={totalPhrases}
+            style="--progress-bg-size: {bgSize}%"
+		></progress>
 	</div>
 </div>
 
@@ -29,4 +43,5 @@
 <script>
     export let processedPhrases = 0;
     export let totalPhrases = 1;
+    $: bgSize = (totalPhrases / processedPhrases) * 100;
 </script>
