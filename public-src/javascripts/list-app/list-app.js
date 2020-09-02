19daad1785dc984d0e352494467a7f57076947af
sheodox <3468630+sheodox@users.noreply.github.com>
2020-09-01 22:03:39 -0500

    Create a distinct scss file for the list file, and specifically only allow it to be as tall as the viewport (so the definitions don't extend beyond the viewport on wide viewports), unlike the other pages where scrolling is fine.

--- public-src/javascripts/list-app/list-app.js
+++ public-src/javascripts/list-app/list-app.js
@@ -1,5 +1,6 @@
 import App from './App.svelte';
 import style from '../../stylesheets/style.scss';
+import listStyle from '../../stylesheets/list.scss';
 
 const app = new App({
 	target: document.querySelector('#app-root')
