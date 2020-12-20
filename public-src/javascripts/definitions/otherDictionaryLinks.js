698b6bf0957757aed991d0dd180d60fb0dfede5a
sheodox <3468630+sheodox@users.noreply.github.com>
2020-12-20 09:12:40 -0600

    Comment out Sanseido. They closed their old dictionary site but plan on replacing it in the future. Sanseido links are all dead links, and the new site is unlikely to have the same URL path structure so it'll probably remain a dead link when the new service launches.

--- public-src/javascripts/definitions/otherDictionaryLinks.js
+++ public-src/javascripts/definitions/otherDictionaryLinks.js
@@ -5,7 +5,9 @@ export const createLinks = searchTerm => {
 		{siteName: 'ALC', href: `http://eow.alc.co.jp/search?q=${encoded}`},
 		{siteName: 'Goo', href: `https://dictionary.goo.ne.jp/srch/all/${encoded}/m0u/`},
 		{siteName: 'Kotobank', href: `https://kotobank.jp/gs/?q=${encoded}`},
-		{siteName: 'Sanseido', href: `https://www.sanseido.biz/User/Dic/Index.aspx?DORDER=&DailyEJ=checkbox&DailyJE=checkbox&DailyJJ=checkbox&TWords=${encoded}&st=0`},
+        //Sanseido stopped their dictionary service as of the end of Sept 2020, but are apparently working on a new version.
+		//So until that new version is available I'm leaving this here as a reminder!
+		// {siteName: 'Sanseido', href: `https://www.sanseido.biz/User/Dic/Index.aspx?DORDER=&DailyEJ=checkbox&DailyJE=checkbox&DailyJJ=checkbox&TWords=${encoded}&st=0`},
 		{siteName: 'Weblio', href: `https://www.weblio.jp/content/${encoded}`},
 		{siteName: 'Google', href: `https://www.google.com/search?q=${encoded}`},
 		{siteName: 'Google Images', href: `https://www.google.com/search?q=${encoded}&tbm=isch`},
