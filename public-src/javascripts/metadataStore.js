fa514044da287caebf534d1a86ed85d1ca26640e
sheodox <3468630+sheodox@users.noreply.github.com>
2021-01-30 22:29:52 -0600

    Ignore the first subscription call. The subscription is used to persist settings changes with a POST to the server, and it was saving the user's settings as soon as the page loads. Now it won't try saving the settings it just got and will only save when the user actually changes something.

--- public-src/javascripts/metadataStore.js
+++ public-src/javascripts/metadataStore.js
@@ -6,7 +6,14 @@ export const settings = writable(window.userMetadata.user.settings);
 
 export const hasAddedPhrases = writable(window.userMetadata.usage.hasAddedPhrases);
 
+let settingsSubscriptionCalled = false;
 settings.subscribe(async settings => {
+	//ignore the store initialization or we'll always save settings as soon as the page loads
+	if (!settingsSubscriptionCalled) {
+		settingsSubscriptionCalled = true;
+		return;
+	}
+
 	await fetch('/user/settings', {
 		method: 'POST',
 		headers: {
