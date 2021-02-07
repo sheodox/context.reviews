024a1f6fc01ce1781334083bea8aeadaab1d856d
sheodox <3468630+sheodox@users.noreply.github.com>
2021-02-06 19:16:21 -0600

    Add a new route /user/stats that will send a number of active phrases for the user and nothing more. This is going to be used by the extension to show the currently active number of phrases on the browser action button as a badge.

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,8 +2,9 @@ import {NextFunction, Response, Router} from 'express';
 import {safeAsyncRoute} from "../middleware/error-handler";
 import {AppRequest} from "../app";
 import {validateSettingsSchema} from "../util/settings";
-import {requireAuth} from "../middleware/route-helpers";
+import {getUserId, requireAuth} from "../middleware/route-helpers";
 import {prisma} from "../util/prisma";
+import {tracker} from "../util/tracker";
 
 const router = Router();
 
@@ -25,6 +26,12 @@ router.post(`/settings`, safeAsyncRoute(async (req: AppRequest, res: Response, n
     })
 
     res.send();
+}));
+
+router.get('/stats', safeAsyncRoute(async (req: AppRequest, res: Response, next: NextFunction) =>  {
+    res.json({
+        activePhrases: await tracker.countActive(getUserId(req))
+    });
 }));
 
 export default router;
